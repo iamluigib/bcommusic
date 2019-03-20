44211b3b72b389d3cf967242d2cb43da60f01c22
Luigi Battista <hello@iamluigib.com>
2019-03-19 20:35:13 -0600

    Started array setup

---
+++ data/songs.js
@@ -0,0 +1,24 @@
+export const songs = [
+  {
+    "title": "Drift",
+    "album": "Urban Flora EP",
+    "artist": [
+      "Alina Baraz",
+      "Galimatias"
+    ],
+    "feat_artist": "none",
+    "release_date": 2015,
+    "album_art": "../images/album-art/urban-flora.jpg"
+  }
+  {
+    "title": "Drift",
+    "album": "Urban Flora EP",
+    "artist": [
+      "Alina Baraz",
+      "Galimatias"
+    ],
+    "feat_artist": "none",
+    "release_date": 2015,
+    "album_art": "../images/album-art/urban-flora.jpg"
+  }
+]
